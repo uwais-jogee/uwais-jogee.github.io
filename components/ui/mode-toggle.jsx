a7777ca311edb408505abbe5d5f48f5c066c4fd0
U Jogee <uj11@student.le.ac.uk>
2025-08-10 19:29:46 +0100

    Updated component files

---
+++ components/ui/mode-toggle.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import * as React from "react"
+import {Moon, Sun} from "lucide-react"
+import {useTheme} from "next-themes"
+
+import {Button} from "@/components/ui/button"
+
+export default function ModeToggle() {
+    const {theme, setTheme} = useTheme()
+
+    // Toggle the theme between "dark" and "light"
+    const toggleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark")
+    }
+
+    return (
+        <Button
+            variant="ghost"
+            size="icon"
+            onClick={toggleTheme}
+            className="cursor-pointer border border-neutral-200/50 supports-backdrop-blur:bg-white/10 dark:border-neutral-700/50 dark:supports-backdrop-blur:bg-black/10 backdrop-blur-md"
+        >
+            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"/>
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    );
+}
