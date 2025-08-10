aa2e851ba63fc5669040e66e90ae5ac68780b121
U Jogee <uj11@student.le.ac.uk>
2025-08-10 21:14:47 +0100

    Updated links to open in new tab. Updated project cards and removed short description template.

--- sections/projects-section.jsx
+++ sections/projects-section.jsx
@@ -16,43 +16,6 @@ export default function ProjectsSection() {
                 <Carousel opts={{slidesToScroll: 1}}>
                     <CarouselContent className="pt-16 pb-12 px-1">
 
-                        {/*Portfrolio Website*/}
-                        <CarouselItem className="md:basis-1/2">
-                            <Card
-                                className="relative overflow-hidden w-full p-0.5 gap-5.5 transition-transform duration-300 hover:scale-[1.01] hover:shadow-lg">
-                                <ShineBorder shineColor={["#22d3ee", "#10b981"]}/>
-                                {/*Image container*/}
-                                <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
-                                    <Image src="/images/portfolio-website.png"
-                                           alt="Screenshot of Portfolio Website"
-                                           layout="fill"
-                                           objectFit="cover"
-                                           className=""
-                                    />
-                                </div>
-                                {/*Title and Description container*/}
-                                <div className="mx-4">
-                                    <CardTitle
-                                        className="text-xl sm:text-2xl font-bold text-neutral-800 dark:text-neutral-100 pb-3">
-                                        Personal Portfolio Website
-                                    </CardTitle>
-                                    <CardContent className="pb-6">
-                                        <div className="flex flex-col gap-3">
-                                            <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
-                                            </CardDescription>
-                                            <div className="flex flex-wrap gap-3">
-                                                <TechIconSmall name="JavaScript" icon={Icons.javaScript}/>
-                                                <TechIconSmall name="Next.js" icon={Icons.nextjs}/>
-                                                <TechIconSmall name="Tailwind CSS" icon={Icons.tailwind}/>
-                                            </div>
-                                        </div>
-                                    </CardContent>
-                                </div>
-                            </Card>
-                        </CarouselItem>
-
                         {/*Placement Management System*/}
                         <CarouselItem className="md:basis-1/2">
                             <Card
@@ -76,8 +39,6 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="Java" icon={Icons.java}/>
@@ -115,11 +76,44 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="Python" icon={Icons.python}/>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </CarouselItem>
+
+                        {/*Portfolio Website*/}
+                        <CarouselItem className="md:basis-1/2">
+                            <Card
+                                className="relative overflow-hidden w-full p-0.5 gap-5.5 transition-transform duration-300 hover:scale-[1.01] hover:shadow-lg">
+                                <ShineBorder shineColor={["#22d3ee", "#10b981"]}/>
+                                {/*Image container*/}
+                                <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
+                                    <Image src="/images/portfolio-website.png"
+                                           alt="Screenshot of Portfolio Website"
+                                           layout="fill"
+                                           objectFit="cover"
+                                           className=""
+                                    />
+                                </div>
+                                {/*Title and Description container*/}
+                                <div className="mx-4">
+                                    <CardTitle
+                                        className="text-xl sm:text-2xl font-bold text-neutral-800 dark:text-neutral-100 pb-3">
+                                        Personal Portfolio Website
+                                    </CardTitle>
+                                    <CardContent className="pb-6">
+                                        <div className="flex flex-col gap-3">
+                                            <CardDescription className="text-sm md:text-base">
+                                            </CardDescription>
+                                            <div className="flex flex-wrap gap-3">
+                                                <TechIconSmall name="JavaScript" icon={Icons.javaScript}/>
+                                                <TechIconSmall name="Next.js" icon={Icons.nextjs}/>
+                                                <TechIconSmall name="Tailwind CSS" icon={Icons.tailwind}/>
                                             </div>
                                         </div>
                                     </CardContent>
@@ -150,8 +144,6 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="Java" icon={Icons.java}/>
@@ -189,8 +181,6 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="C++" icon={Icons.cpp}/>
@@ -224,8 +214,6 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="HTML" icon={Icons.html}/>
@@ -261,8 +249,6 @@ export default function ProjectsSection() {
                                     <CardContent className="pb-6">
                                         <div className="flex flex-col gap-3">
                                             <CardDescription className="text-sm md:text-base">
-                                                A short and concise summary of what this project is about and the
-                                                technologies used.
                                             </CardDescription>
                                             <div className="flex flex-wrap gap-3">
                                                 <TechIconSmall name="Python" icon={Icons.python}/>
