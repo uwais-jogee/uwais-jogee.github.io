642516beb7a81cc6699b0f96c5cb64bbe64fb19a
Uwais Jogee <124700940+uwais-jogee@users.noreply.github.com>
2025-10-27 15:18:45 +0000

    Use memo directly from React

--- components/layout/tech-icon.jsx
+++ components/layout/tech-icon.jsx
@@ -1,7 +1,7 @@
 import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
 import {memo} from "react";
 
-export const TechIcon = React.memo(function TechIcon({ name, icon: IconComponent }) {
+export const TechIcon = memo(function TechIcon({ name, icon: IconComponent }) {
     return (
         <Tooltip>
             <TooltipTrigger asChild>
