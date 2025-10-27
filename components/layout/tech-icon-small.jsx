d50f3d71f365372a8418a21407a8a77f6968344f
Uwais Jogee <124700940+uwais-jogee@users.noreply.github.com>
2025-10-27 15:01:59 +0000

    Refactor TechIconSmall to use React.memo

--- components/layout/tech-icon-small.jsx
+++ components/layout/tech-icon-small.jsx
@@ -1,19 +1,18 @@
 import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
 import {useState} from "react";
 
-export function TechIconSmall({name, icon: IconComponent}) {
-    const [isOpen, setIsOpen] = useState(false);
-
+export function TechIconSmall = React.memo(function TechIconSmall({ name, icon: IconComponent}) {
     return (
-        <Tooltip open={isOpen} onOpenChange={setIsOpen}>
+        <Tooltip>
             <TooltipTrigger asChild>
-                <div
-                    onClick={() => setIsOpen(!isOpen)}
+                <button
+                    type="button"
+                    aria-label={name}
                     className="flex size-8 items-center justify-center rounded-full bg-transparent transition-colors hover:bg-neutral-100 dark:hover:bg-neutral-800 sm:size-9"
                 >
                     <IconComponent
                         className="size-5 text-neutral-800 transition-colors dark:text-neutral-200 sm:size-6"/>
-                </div>
+                </button>
             </TooltipTrigger>
             <TooltipContent>
                 <p>{name}</p>
